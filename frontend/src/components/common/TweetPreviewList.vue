cc21bac9b550cf2a4dbcef9b5105c73a7b8719f5
Volodymyr <tumko.volodymyr@gmail.com>
2019-06-17 19:50:27 +0300

    tweet card view was done #16

--- frontend/src/components/common/TweetPreviewList.vue
+++ frontend/src/components/common/TweetPreviewList.vue
@@ -1,13 +1,33 @@
 <template>
     <div class="tweets-container">
-        <transition-group name="slide-prev" tag="div">
+        <div class="row">
+            <div class="col-sm text-center">
+                <span class="align-middle">
+                    Card view
+                </span>
+                <span class="align-middle">
+                    <label class="switch">
+                        <input v-model="isCardTweetView" type="checkbox">
+                        <span class="slider round"></span>
+                    </label>
+                </span>
+            </div>
+        </div>
+        <transition-group
+            name="slide-prev"
+            tag="div"
+            :class="{
+                'flex': isCardTweetView
+            }"
+        >
             <template v-for="tweet in tweets">
-                <TweetPreview
+                <component
+                    v-bind:is="currentTweetViewComponent"
                     :key="tweet.id"
                     :tweet="tweet"
                     :isCommented="tweetIsCommented(tweet.id)"
                     @click="onTweetClick"
-                />
+                ></component>
             </template>
         </transition-group>
         <infinite-loading @infinite="infiniteHandler">
@@ -21,6 +41,7 @@
 <script>
 import InfiniteLoading from 'vue-infinite-loading';
 import TweetPreview from './TweetPreview.vue';
+import TweetCardPreview from './TweetCardPreview.vue';
 
 export default {
     name: 'TweetPreviewList',
@@ -35,6 +56,10 @@ export default {
             required: true
         },
     },
+
+    data: () => ({
+        isCardTweetView: false
+    }),
 
     components: {
         TweetPreview,
@@ -54,11 +79,91 @@ export default {
             return this.commentedTweets.indexOf(tweetId) > -1;
         },
     },
+
+    computed: {
+        currentTweetViewComponent() {
+            return this.isCardTweetView ? TweetCardPreview : TweetPreview;
+        }
+    }
 };
 </script>
 
 <style scoped lang="scss">
 .tweets-container {
     padding-bottom: 20px;
+}
+/* The switch - the box around the slider */
+.switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+}
+
+/* The slider */
+.slider {
+    position: absolute;
+    cursor: pointer;
+    top: 4px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+}
+
+.slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 2px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+}
+
+input:checked + .slider {
+    background-color: #14bc61;
+}
+
+input:focus + .slider {
+    box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+    border-radius: 34px;
+}
+
+.slider.round:before {
+    border-radius: 50%;
+}
+
+.col-sm.text-center {
+    display: flex;
+    align-items: center;
+}
+span.align-middle {
+    margin-right: 5px;
+}
+.flex {
+    display: flex;
+    flex-wrap: wrap;
 }
 </style>
